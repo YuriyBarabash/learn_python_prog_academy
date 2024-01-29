5ff5241bb70e20407529f53c4d83a7646382abd4
Vostro <barabashyuriy@gmail.com>
2024-01-29 19:00:31 +0100

    completed homework Regular_expressions

--- homework_DJANGO_regular_expretions.py
+++ homework_DJANGO_regular_expretions.py
@@ -17,5 +17,16 @@ def credit_card(card_number):
     return False
 
 # Task 3
+def check_email(email):
+    if re.match(r'^[a-zA-Z0-9_]+[-]?[a-zA-Z0-9_]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+        return True
+    return False
+
+# Task 4
+def check_login(login):
+    if re.match(r'^[a-zA-Z0-9]{2,10}$', login):
+        return True
+    return False
+
 
 
